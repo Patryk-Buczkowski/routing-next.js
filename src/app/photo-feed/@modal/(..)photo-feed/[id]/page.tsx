7e59426fc4a96df4001b2c6dbff457ff551e1aca
Patryk-Buczkowski <132567382+Patryk-Buczkowski@users.noreply.github.com>
2024-12-26 21:32:32 +0100

    Update page.tsx

--- src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
+++ src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
@@ -1,37 +1,26 @@
 import Image from 'next/image';
-import wondersImages, { WonderImage } from '../../../wonders';
-import Modal from '@/components/modal';
 import { notFound } from 'next/navigation';
+import wondersImages from '../../../wonders';
 
-type Params = {
-  params: Promise<{ id: string }>;
-};
+export default async function PhotoModal({ params }: { params: { id: string } }) {
+  const { id } = params;
 
-export async function generateStaticParams() {
-  return [
-    { id: '1' },
-  ];
-}
+  const photo = wondersImages.find(p => p.id === id); 
 
-
-export default async function PhotoModal({ params }: Params) {
-  const { id } = await params;
-  const photo: WonderImage = wondersImages.find(p => p.id === id)!;
-
-  if (photo === undefined) {
-    return notFound();
+  if (!photo) {
+    return notFound(); 
   }
 
   return (
-    <Modal>
+    <Modal> 
       <Image
-        alt={photo.name}
-        src={photo.src}
-        className="w-full object-cover aspect-square"
+        src={photo.src} 
+        alt={photo.name} 
+        className="w-full object-cover aspect-square" 
       />
 
       <div className="bg-white p-4">
-        <h2 className="text-xl font-semibold">{photo.name}</h2>
+        <h2>{photo.name}</h2>
         <h3>{photo.photographer}</h3>
         <h3>{photo.location}</h3>
       </div>
