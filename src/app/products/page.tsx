cdd4b663113f39ddcdc4aaaade1d96118a415367
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-12-26 21:52:28 +0100

    Remove commented-out error simulation code from Products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,9 +3,9 @@
 import { MyButton } from '@/components/buttons/Button';
 import { useRouter } from 'next/navigation';
 
-function getRandomError(number: number) {
-  return Math.floor(Math.random() * number);
-}
+// function getRandomError(number: number) {
+//   return Math.floor(Math.random() * number);
+// }
 
 export default function Products() {
   const router = useRouter();
@@ -18,15 +18,15 @@ export default function Products() {
     'product 6',
   ];
 
-  const randomError = getRandomError(2);
+  // const randomError = getRandomError(2);
 
   return (
     <div className="flex flex-col w-fit gap-2">
       <h1>Product list</h1>
       {products.map((product, index) => {
-        if (randomError === 1) {
-          throw new Error('Simulated error ;)')
-        }
+        // if (randomError === 1) {
+        //   throw new Error('Simulated error ;)')
+        // }
         return (
           <MyButton
             key={product}
