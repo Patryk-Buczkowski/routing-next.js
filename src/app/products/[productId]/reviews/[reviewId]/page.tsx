3c6071831f947559964a7a84c8ee278fafe73ad9
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-12-26 17:31:35 +0100

    Add generateStaticParams function to review page for static generation

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -12,11 +12,13 @@ type Props = {
   }>;
 };
 
-// export async function generateStaticParams() {
-//   return [
-//     { slug: ['1'] },
-//   ];
-// }
+export async function generateStaticParams() {
+  return [
+    { reviewId: '1',
+      productId: '1'
+     },
+  ];
+}
 
 export default function ReviewDetail({ params }: Props) {
   const [product, setProduct] = useState(0);
