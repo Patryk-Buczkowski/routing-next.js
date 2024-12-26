e4f74c3fd4b2bdee8d97c2830a9cc18ac98ede96
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-12-26 22:06:31 +0100

    add priority to image

--- src/app/photo-feed/page.tsx
+++ src/app/photo-feed/page.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import wonders from "./wonders";
-import Image from "next/image";
+import Link from 'next/link';
+import wonders from './wonders';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -10,8 +10,12 @@ export default function Home() {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {wonders.map(({ id, src, name }) => (
-          <Link key={id} href={`/photo-feed/${id}`}>
+          <Link
+            key={id}
+            href={`/photo-feed/${id}`}
+          >
             <Image
+              priority
               alt={name}
               src={src}
               className="w-full object-cover aspect-square"
